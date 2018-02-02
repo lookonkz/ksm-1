4bc37f4b797f92bf0f608a180ed90c5283df4e3f
user1 <user1@iMac-user1.local>
2018-02-02 16:31:42 +0600

    first commit

---
+++ inc/sendEmail.php
@@ -0,0 +1,96 @@
+﻿<?php
+
+// Replace this with your own email address
+$siteOwnersEmail = 'user@website.com';
+
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['contactName']));
+   $email = trim(stripslashes($_POST['contactEmail']));
+   $phone = trim(stripslashes($_POST['contactSubject']));
+   $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+
+   // Check Name
+	if (strlen($name) < 2) {
+		$error['name'] = "Please enter your name.";
+	}
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address.";
+	}
+	// Check Message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	}
+   // Subject
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+
+  $token = "518245891:AAHJJbg1NL0nI2En1VVD_UcZED2Kiq9sydA";
+  // $chat_id = "518245891";
+  $chat_id = "-294528960";
+  $values = $hook->getValues();
+  $ip = $modx->getOption('REMOTE_ADDR', $_SERVER, '');
+  #Создаем массив
+  $arr = array(
+  "Имя" => $name,
+  "Телефон" => $phone,
+  "Email" => $email,
+  "Сообщение" => $contact_message,
+  "Айпи" => $ip);
+
+  /*Цикл по массиву (собираем сообщение) */
+  foreach($arr as $key => $value) {
+       $txt .= "<b>".$key."</b>: ".$value."%0A";
+    }
+
+  #Отправляем сообщение
+  $fp=fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+  #Возвращаем true
+  return true;
+
+
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+	$message .= "Email address: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= $contact_message;
+   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+   if (!$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+
+		echo $response;
+
+	} # end if - there was a validation error
+
+}
+
+?>
